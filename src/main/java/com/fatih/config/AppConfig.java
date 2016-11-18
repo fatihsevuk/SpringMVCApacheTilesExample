6557c88d7930fe08c46695281efc6683b34bfa34
fthsvk <bmfthsvk@gmail.com>
2016-11-18 16:13:17 +0200

    spring mvc apache tiles

---
+++ src/main/java/com/fatih/config/AppConfig.java
@@ -0,0 +1,51 @@
+package com.fatih.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages="com.fatih")
+public class AppConfig extends WebMvcConfigurerAdapter {
+	
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer(){
+	
+		TilesConfigurer tilesConfigurer=new TilesConfigurer();
+		
+		tilesConfigurer.setDefinitions(new String[]{"/WEB-INF/views/**/tiles.xml"});
+		tilesConfigurer.setCheckRefresh(true);
+		
+		return tilesConfigurer;
+		
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		TilesViewResolver viewResolver=new TilesViewResolver();
+		registry.viewResolver(viewResolver);
+		
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+	
+	
+	
+}
